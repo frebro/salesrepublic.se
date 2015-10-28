cf63e010eb865d8aabe44ee68fda95d12a9e722e
Fredrik Broman <frebro@gmail.com>
2015-10-28 23:12:01 +0100

    Set up basic Tour control interactions

--- src/scripts/sr.tour.js
+++ src/scripts/sr.tour.js
@@ -19,6 +19,7 @@
   function Plugin (element, options) {
     this.options = $.extend(true, $.fn[NAMESPACE].defaults, options);
     this.$element = $(element);
+    this.$locationControls = $('[data-target]', this.$element);
   }
 
 
@@ -36,8 +37,67 @@
      * @api public
      */
     init: function () {
-      console.log(this.$element);
-    }
+
+      // Set up interactions for location controls
+      this.$locationControls.on('mouseenter', $.proxy(this.enterLocationControl, this));
+      this.$locationControls.on('mouseleave', $.proxy(this.leaveLocationControl, this));
+      this.$locationControls.on('focusin', $.proxy(this.focusLocationControl, this));
+      this.$locationControls.on('focusout', $.proxy(this.blurLocationControl, this));
+    },
+
+    /**
+     * Mouse Enter Location Control method
+     */
+    enterLocationControl: function(event) {
+      var $control = $(event.target);
+      var targetId = $control.data('target');
+
+      // Highlight any control with the targetId
+      this.$locationControls.filter('[data-target="' + targetId + '"]').css('background-color', 'salmon');
+    },
+
+    /**
+     * Mouse Leave Location Control method
+     */
+    leaveLocationControl: function(event) {
+      var $control = $(event.target);
+      var targetId = $control.data('target');
+
+      // Remove highlight from any control with the targetId
+      this.$locationControls.filter('[data-target="' + targetId + '"]').css('background-color', '');
+    },
+
+    /**
+     * Focus Location Control method
+     */
+    focusLocationControl: function(event) {
+      var $control = $(event.target);
+      var targetId = $control.data('target');
+      var $target = $('#' + targetId);
+
+      // Highlight any control with the targetId
+      this.$locationControls.filter('[data-target="' + targetId + '"]').css('background-color', 'salmon');
+
+      // Make currently active target inactive
+      if (this.options.activeTarget) {
+        this.options.activeTarget.css('background-color', '');
+      }
+
+      // Set target to active
+      $target.css('background-color', 'gold');
+      this.options.activeTarget = $target;
+    },
+
+    /**
+     * Blur Location Control method
+     */
+    blurLocationControl: function(event) {
+      var $control = $(event.target);
+      var targetId = $control.data('target');
+
+      // Remove highlight from any control with the targetId
+      this.$locationControls.filter('[data-target="' + targetId + '"]').css('background-color', '');
+    },
 
   };
 
@@ -65,7 +125,9 @@
    * @type {Object}
    * @api public
    */
-  $.fn[NAMESPACE].defaults = {};
+  $.fn[NAMESPACE].defaults = {
+    activeTarget: null
+  };
 
   /**
    * jQuery plugin data api
