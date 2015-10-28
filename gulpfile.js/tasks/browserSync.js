4acb89ef0b50d146ec81e9753144b73b2b71e40b
Fredrik Broman <frebro@gmail.com>
2015-10-28 20:16:25 +0100

    Add Tour map section

--- gulpfile.js/tasks/browserSync.js
+++ gulpfile.js/tasks/browserSync.js
@@ -1,6 +1,6 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
-var config      = require('../config/browserSync')
+var config      = require('../config/browserSync');
 
 gulp.task('browserSync', function() {
   return browserSync(config);
