7d365f488fc2ec0ae5dd734f2d5fa7672f04741a
Fredrik Broman <frebro@gmail.com>
2015-10-28 21:38:56 +0100

    Init tour component js

---
+++ src/scripts/sr.tour.js
@@ -0,0 +1,80 @@
+;(function(window, document, $, undefined) {
+
+  'use strict';
+
+  /**
+   * Plugin NAMESPACE
+   * @type {String}
+   * @api private
+   */
+  var NAMESPACE = 'im.tour',
+      SELECTOR = '.tour';
+
+  /**
+   * Plugin constructor
+   * @param {Node} element
+   * @param {Object} [options]
+   * @api public
+   */
+  function Plugin (element, options) {
+    this.options = $.extend(true, $.fn[NAMESPACE].defaults, options);
+    this.$element = $(element);
+  }
+
+
+  /**
+   * Plugin prototype
+   * @type {Object}
+   * @api public
+   */
+  Plugin.prototype = {
+    constructor: Plugin,
+    version: '0.1.0',
+
+    /**
+     * Init method
+     * @api public
+     */
+    init: function () {
+      console.log(this.$element);
+    }
+
+  };
+
+  /**
+   * jQuery plugin definition
+   * @param  {String} [method]
+   * @param  {Object} [options]
+   * @return {Object}
+   * @api public
+   */
+  $.fn[NAMESPACE] = function (method, options) {
+    return this.each(function () {
+      var $this = $(this),
+      data = $this.data(NAMESPACE);
+      options = (typeof method === 'object') ? method : options;
+      if (!data) {
+        $this.data(NAMESPACE, (data = new Plugin(this, options)));
+      }
+      data[(typeof method === 'string') ? method : 'init']();
+    });
+  };
+
+  /**
+   * jQuery plugin defaults
+   * @type {Object}
+   * @api public
+   */
+  $.fn[NAMESPACE].defaults = {};
+
+  /**
+   * jQuery plugin data api
+   * @api public
+   */
+  $(window).on('load', function (event) {
+    $(SELECTOR).each(function () {
+      $(this)[NAMESPACE]();
+    });
+  });
+
+}(this, this.document, this.jQuery));
